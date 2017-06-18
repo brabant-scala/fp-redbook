c41633db070231335dc70ade3d49b367d9758ec5
Ardjan Dommisse <ardjan@gmail.com>
2017-06-18 10:08:27 +0200

    Exercises ch 9: 9

--- src/main/scala/nl/hugo/redbook/ch9/Parsers.scala
+++ src/main/scala/nl/hugo/redbook/ch9/Parsers.scala
@@ -1,5 +1,7 @@
 package nl.hugo.redbook.ch9
 
+import java.util.regex.Pattern
+
 import org.scalacheck.{Gen, Prop}
 
 import scala.language.{higherKinds, implicitConversions}
@@ -33,7 +35,7 @@ trait Parsers[Parser[+ _]] {
   implicit def regex(r: Regex): Parser[String]
 
   // Exercise 9.6
-  def toNumber:Parser[Int] =
+  def toNumber: Parser[Int] =
     for {
       v <- "[0-9]+".r
       n = v.toInt
@@ -54,7 +56,7 @@ trait Parsers[Parser[+ _]] {
   def many[A](p: Parser[A]): Parser[List[A]] = p.map2(p.many)(_ :: _) or succeed(List.empty)
 
   // Exercise 9.8
-  def map[A, B](a: Parser[A])(f: A => B): Parser[B] = a.flatMap( b => succeed(f(b)) )
+  def map[A, B](a: Parser[A])(f: A => B): Parser[B] = a.flatMap(b => succeed(f(b)))
 
   // This is the parser's variant of 'unit'
   def succeed[A](a: A): Parser[A] = string("") map (_ => a)
@@ -62,18 +64,18 @@ trait Parsers[Parser[+ _]] {
   def slice[A](p: Parser[A]): Parser[String]
 
   //Exercise 9.6
-  def map2[A,B,C](p: Parser[A], p2: => Parser[B])(f: (A,B) => C): Parser[C] =
+  def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
     for {
       a <- p
       b <- p2
-    } yield f(a,b)
+    } yield f(a, b)
 
   //Exercise 9.6
   def product[A, B](p: Parser[A], p2: Parser[B]): Parser[(A, B)] =
     for {
       a <- p
       b <- p2
-    } yield (a,b)
+    } yield (a, b)
 
   // Exercise 9.1 & 9.5
   def map2_9_1[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] = (p ** p2) map f.tupled
@@ -82,6 +84,65 @@ trait Parsers[Parser[+ _]] {
   def many1[A](p: Parser[A]): Parser[List[A]] = p.map2(p.many)(_ :: _)
 
   def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+  // Exercise 9.9
+  def whiteSpaces: Parser[List[String]] = (" " | "\n" | "\r" | "\t").many
+
+  // Exercise 9.9
+  def dropLeft[A, B](l: Parser[A], r: Parser[B]): Parser[B] =
+    for {
+      _ <- l
+      v <- r
+    } yield v
+
+  // Exercise 9.9
+  def dropRight[A, B](l: Parser[A], r: Parser[B]): Parser[A] =
+    for {
+      v <- l
+      _ <- r
+    } yield v
+
+  // Exercise 9.9
+  def surround[A, B, C](p: Parser[A], l: Parser[B], r: Parser[C]): Parser[A] =
+    l <| p |> r
+
+  // Exercise 9.9
+  def eof: Parser[String] =
+    regex("\\z".r)
+
+  // Exercise 9.9
+  def root[A](p: Parser[A]): Parser[A] = p |> eof
+
+  // Exercise 9.9
+  def sep1[A, B](p: Parser[A], s: Parser[B]): Parser[List[A]] =
+    for {
+      f <- p
+      r <- (s <| p).many
+    } yield f :: r
+
+  // Exercise 9.9
+  def sep[A, B](p: Parser[A], s: Parser[B]): Parser[List[A]] =
+    p sep1 s or succeed(List.empty)
+
+  // Exercise 9.9
+  def thru(s: String): Parser[String] = (".*?" + Pattern.quote(s)).r
+
+  // Exercise 9.9
+  /** Unescaped string literals, like "foo" or "bar". */
+  def quoted: Parser[String] = string("\"") <| thru("\"").map(_.dropRight(1))
+
+  // Exercise 9.9
+  /** Floating point literals, converted to a `Double`. */
+  def double: Parser[Double] =
+    doubleString map (_.toDouble)
+
+  // Exercise 9.9
+  def doubleString: Parser[String] =
+    token("[-+]?([0-9]*\\.)?[0-9]+([eE][-+]?[0-9]+)?".r)
+
+  // Exercise 9.9
+  def token[A](p: Parser[A]): Parser[A] =
+    p |> whiteSpaces
 
   implicit class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
@@ -105,6 +166,18 @@ trait Parsers[Parser[+ _]] {
     def map2[B, C](p2: => Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
 
     def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
+
+    def <|[B](r: Parser[B]): Parser[B] = self.dropLeft(p, r)
+
+    def |>[B](r: Parser[B]): Parser[A] = self.dropRight(p, r)
+
+    def surround[B, C](l: Parser[B], r: Parser[C]): Parser[A] = self.surround(p, l, r)
+
+    def as[B](b: B): Parser[B] = self.map(self.slice(p))(_ => b)
+
+    def sep[B](s: Parser[B]): Parser[List[A]] = self.sep(p, s)
+
+    def sep1[B](s: Parser[B]): Parser[List[A]] = self.sep1(p, s)
   }
 
   object Laws {
@@ -176,4 +249,56 @@ case class ParseError(
                        stack: List[(Location, String)] = List(),
                        otherFailures: List[ParseError] = List()
                      ) {
+}
+
+trait JSON
+
+object JSON {
+
+  case object JNull extends JSON
+
+  case class JNumber(get: Double) extends JSON
+
+  case class JString(get: String) extends JSON
+
+  case class JBool(get: Boolean) extends JSON
+
+  case class JArray(get: IndexedSeq[JSON]) extends JSON
+
+  case class JObject(get: Map[String, JSON]) extends JSON
+
+  def jsonParser[Err, Parser[+ _]](P: Parsers[Parser]): Parser[JSON] = {
+    import P._
+
+    // A value can be a string in double quotes, or a number, or true or false or null, or an object or an array.
+    // These structures can be nested.
+    def value: Parser[JSON] = {
+      "null".as(JNull) |
+        double.map(JNumber) |
+        quoted.map(JString) |
+        "true".as(JBool(true)) |
+        "false".as(JBool(false)) |
+        obj |
+        array
+    }
+
+    // An object is an unordered set of name/value pairs. An object begins with { (left brace) and ends with }
+    // (right brace). Each name is followed by : (colon) and the name/value pairs are separated by , (comma).
+    def obj: Parser[JObject] = {
+      ((quoted ** (":" <| value)) sep "," map (kvs => JObject(kvs.toMap)))
+        .surround("{", "}")
+    }
+
+    // An array is an ordered collection of values. An array begins with [ (left bracket) and ends with ]
+    // (right bracket). Values are separated by , (comma).
+    def array: Parser[JArray] = {
+      (value sep "," map (vs => JArray(vs.toIndexedSeq)))
+        .surround("[", "]")
+    }
+    // JSON is built on two structures:
+    //  * A collection of name/value pairs. In various languages, this is realized as an object, record, struct,
+    //    dictionary, hash table, keyed list, or associative array.
+    //  * An ordered list of values. In most languages, this is realized as an array, vector, list, or sequence.
+    root(obj | array)
+  }
 }
